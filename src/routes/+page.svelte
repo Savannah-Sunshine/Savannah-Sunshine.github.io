a7528f4f9ef2b882ae749802a83ba2aa0410845b
Savannah-Sunshine <s.ella@me.com>
2024-06-27 17:22:14 -0600

    feat: initial status svelte setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
   import { base } from "$app/paths";
 </script>
 
+
+<!-- prepend relative paths with the SvelteKit base path so that your app can build successfully for production. -->
 <h1>sveltekit-gh-pages</h1>
 <p>Deployed to GitHub Pages.</p>
 <a href="{base}/about">About</a>
