cf6d0742cdf183a65961e410f5db0befb861e138
Savannah-Sunshine <s.ella@me.com>
2024-07-08 17:12:44 -0600

    feat: add pico

--- src/+layout.ts
+++ src/+layout.ts
@@ -1,3 +1,8 @@
 // Will prerender all pages
 export const prerender = true
 
+// disable server side rendering and makes a SINGLE PAGE APP
+export const ssr = false;
+
+// will make sure pico is always there
+// import "@picocss/pico/css/pico.css"
